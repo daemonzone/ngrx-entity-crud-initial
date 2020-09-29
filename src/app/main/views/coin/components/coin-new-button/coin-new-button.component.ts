40bf1c1ab1a34b74ae68bd1262c3271dfdad1a2f
davide.villani <davide.villani@altran.it>
2020-09-29 11:40:54 +0200

    + Added New Coin button (working)

--- src/app/main/views/coin/components/coin-new-button/coin-new-button.component.ts
+++ src/app/main/views/coin/components/coin-new-button/coin-new-button.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PopUpData} from '@root-store/router-store/pop-up-base.component';
+import {Coin} from '@models/vo/coin';
+import {RouterStoreActions} from '@root-store/router-store';
+import {Store} from '@ngrx/store';
+import {CoinStoreActions, RootStoreState} from '@root-store/index';
+import {Actions} from 'ngrx-entity-crud';
 
 @Component({
-  selector: 'app-coin-new-button',
+  selector: 'coin-new-button',
   templateUrl: './coin-new-button.component.html',
-  styleUrls: ['./coin-new-button.component.css']
+  styleUrls: []
 })
 export class CoinNewButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly store$: Store<RootStoreState.State>) {
+  }
 
   ngOnInit() {
   }
 
+
+  onNew() {
+    console.log('CoinListComponent.onEdit()');
+
+    const data: PopUpData<Coin> = {
+      item: new Coin(),
+      props: {title: 'New Coin', route: 'coin'}
+    };
+
+    // apro la popUP
+    this.store$.dispatch(RouterStoreActions.RouterGoPopUp({
+      path: ['coin', {outlets: {popUp: ['edit']}}],
+      data
+    }));
+
+  }
 }
